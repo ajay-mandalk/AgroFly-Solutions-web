943cdc97dd78c98a046e9d41d0fc45cced3f8c42
Ajay <ajaykumar84842@gamil.com>
2025-06-20 17:25:01 +0530

    Refactor components: Remove unused sections and optimize VelocityAnnoucer velocity. Clean up layout by deleting redundant files and enhancing the Hero section with a new structure. This improves maintainability and performance.

--- src/config/announcement.ts
+++
@@ -1,8 +0,0 @@
-// src/config/announcement.ts
-export const announcement = {
-    isActive: true,
-    text: "New: Get Insights Smart Agriculture and Farming",
-    link: "/learn",
-    badge: "Get Insights",
-    endDate: "2024-04-30"
-  } as const;
